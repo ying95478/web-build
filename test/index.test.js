59b261389c594c13b061a24c0745460a8e7f87b6
chenguopeng <l>
2021-04-11 22:14:47 +0800

    first commit

--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-const webBuild = require('..')
-
-// TODO: Implement module test
-test('web-build', () => {
-  expect(webBuild('w')).toBe('w@zce.me')
-  expect(webBuild('w', { host: 'wedn.net' })).toBe('w@wedn.net')
-  expect(() => webBuild(100)).toThrow('Expected a string, got number')
-})
